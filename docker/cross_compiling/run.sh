02bc40928f7864c288fa62c71873c4acf8496c7c
Mislav Novakovic <mislav.novakovic@sartura.hr>
2019-02-20 12:43:48 +0100

    docker cross: use libssh 0.7.7
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- docker/cross_compiling/run.sh
+++ docker/cross_compiling/run.sh
@@ -6,7 +6,7 @@ if [ ! -d "x86_sysrepo" ]; then
     docker create -ti --name tmp_docker_image sysrepo/sysrepo-netopeer2:latest bash
     docker cp tmp_docker_image:/usr/local/bin x86_sysrepo
     docker cp tmp_docker_image:/usr/local/lib x86_sysrepo
-    docker rm -fv tmp_docker_image
+    docker rm tmp_docker_image
 fi
 
 docker build -t sysrepo/cross -f Dockerfile.cross .
@@ -18,6 +18,6 @@ fi
 mkdir cross_sysrepo
 docker create -ti --name cross_docker_image sysrepo/cross bash
 sudo docker cp cross_docker_image:/opt/dev/root cross_sysrepo
-docker rm -fv cross_docker_image
+docker rm cross_docker_image
 
 docker run -v ${PWD}:/copy_dir -ti --rm sysrepo/sysrepo-netopeer2 bash /copy_dir/patchelf.sh
