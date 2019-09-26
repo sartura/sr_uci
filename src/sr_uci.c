943fe54e0fe66d5ac6fb6c3dfdcd5e604b105043
Luka Paulic <luka.paulic0@gmail.com>
2019-09-26 14:11:12 +0200

    Update sysrepo API

--- src/sr_uci.c
+++ src/sr_uci.c
@@ -796,7 +796,7 @@ int sr_uci_init_data(sr_ctx_t *ctx, const char *uci_config,
   }
 
   /* commit the changes to startup datastore */
-  rc = sr_apply_changes(ctx->startup_sess);
+  rc = sr_apply_changes(ctx->startup_sess, 0);
   CHECK_RET(rc, cleanup, "failed sr_apply_changes: %s", sr_strerror(rc));
 
   return rc;
